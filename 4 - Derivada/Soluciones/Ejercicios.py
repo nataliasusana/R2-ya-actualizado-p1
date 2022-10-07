561340706988c431a70a5c493faeaf1ab5139f54
Majd <majd0397@gmail.com>
2022-10-07 16:25:42 -0500

    Actualizacion de Derivada

--- 4 - Derivada/Soluciones/Ejercicios.py
+++ 4 - Derivada/Soluciones/Ejercicios.py
@@ -1,18 +1,33 @@
 import numpy as np
 from Soluciones.Manager import Actividad
 
-def Ejercicio_1a():
-    S1=np.load("./Soluciones/sol01.npy")
-    Actividad("F",S1).Ejercicio()
+class Ejercicios:
+    def __init__(self,Num,Let):
+        self.Num=Num
+        self.Let=Let
+        
+    def Solucion(self):
+        cadena=str("./Soluciones/sol0"+str(self.Num)+self.Let+".npy")
+        S=np.load(cadena)
+        if self.Let == "a":
+            Actividad("F",S).Ejercicio()
+        if self.Let == "b":
+            Actividad("G",S).Ejercicio()
+        if self.Let == "c":
+            Actividad("H",S).Ejercicio()
 
-def Ejercicio_1b():
-    S2=np.load("./Soluciones/sol02.npy")
-    Actividad("G",S2).Ejercicio()
-
-def Ejercicio_2a():
-    S3=np.load("./Soluciones/sol03.npy")
-    Actividad("F",S3).Ejercicio()
-    
-def Ejercicio_2b():
-    S4=np.load("./Soluciones/sol04.npy")
-    Actividad("G",S4).Ejercicio()
+Ejercicio_1a=Ejercicios(1,"a").Solucion
+Ejercicio_1b=Ejercicios(1,"b").Solucion
+Ejercicio_2a=Ejercicios(2,"a").Solucion
+Ejercicio_2b=Ejercicios(2,"b").Solucion
+Ejercicio_3a=Ejercicios(3,"a").Solucion
+Ejercicio_3b=Ejercicios(3,"b").Solucion
+Ejercicio_4a=Ejercicios(4,"a").Solucion
+Ejercicio_4b=Ejercicios(4,"b").Solucion
+Ejercicio_4c=Ejercicios(4,"c").Solucion
+Ejercicio_5a=Ejercicios(5,"a").Solucion
+Ejercicio_5b=Ejercicios(5,"b").Solucion
+Ejercicio_6a=Ejercicios(6,"a").Solucion
+Ejercicio_6b=Ejercicios(6,"b").Solucion
+Ejercicio_7a=Ejercicios(7,"a").Solucion
+Ejercicio_7b=Ejercicios(7,"b").Solucion
--- 4 - Derivada/Soluciones/Manager.py
+++ 4 - Derivada/Soluciones/Manager.py
@@ -14,13 +14,12 @@ class Actividad:
     
     def Ejercicio(self):
         R1 = self.Respuesta()
-        
-        for i in range(len(self.S)):
-            self.S[i]=self.S[i].replace(" ","")
-
+        R1 = R1.replace(" ","")
+        R1 = R1.replace("*","")
         aux=False
-        for i in self.S:
-            if R1.replace(" ","") == i:
+
+        for i in self.S:            
+            if R1 == i:
                 aux=True
         if aux:
             print(colored("-"*100,"green"))
@@ -28,4 +27,6 @@ class Actividad:
         else:
             print(colored("-"*100,"red"))
             print(colored("Tu respuesta es incorrecta, verifica las reglas de la derivacion y vuelve a intentar","red"))
+            
+
             
